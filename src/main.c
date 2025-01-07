15f5025bf8d5f36e93a31af45eae4b9e8596e4fd
peethree <pieterschoptkont@gmail.com>
2025-01-07 16:10:07 +0100

    removed comment

--- src/main.c
+++ src/main.c
@@ -1,12 +1,3 @@
-/*
-Raylib example file.
-This is an example main file for a simple raylib project.
-Use this as a starting point or replace it with your code.
-
-by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit https://creativecommons.org/publicdomain/zero/1.0/
-
-*/
-
 #include "raylib.h"
 
 #include "resource_dir.h"	// utility header for SearchAndSetResourceDir
